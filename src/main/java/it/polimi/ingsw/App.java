335582b57e0948920693808d8a0dd5b639905644
hamzahaddaoui <hamza.haddaoui@mail.polimi.it>
2020-03-16 16:57:06 +0100

    First commit - Simple "Hello World" project

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -1,13 +1,9 @@
 package it.polimi.ingsw;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main (String[] args){
+        boolean [][][] map = new boolean[5][5][3];
+        
+        System.out.println("Hello World!");
     }
 }
