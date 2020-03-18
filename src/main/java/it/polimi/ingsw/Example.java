9e5143ba9eadf96c568b94977b722771005f536a
giusti-leo <leonardo.giusti@mail.polimi.it>
2020-03-18 16:10:19 +0100

    leo1

--- src/main/java/it/polimi/ingsw/Example.java
+++ src/main/java/it/polimi/ingsw/Example.java
@@ -4,7 +4,11 @@ public class Example {
     public static void main (String[] args){
         boolean [][][] map = new boolean[5][5][3];
         System.out.println("Hello World prova Dario!");
+
         System.out.println("Hello World prova Dario!");
+
+
+
         System.out.println("Hello World prova Dario!");
         System.out.println("Hello World prova Dario!");
 
