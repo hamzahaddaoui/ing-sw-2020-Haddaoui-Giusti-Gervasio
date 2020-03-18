2ce7a0c637a8882febe287a2e1377067c0adbe0a
Vasio1298 <darioalex.gervasio@mail.polimi.it>
2020-03-18 15:54:23 +0100

    Merge branch 'master' of C:\Users\Vasio\IdeaProjects\ing-sw-2020-Haddaoui-Giusti-Gervasio with conflicts.

--- src/main/java/it/polimi/ingsw/Example.java
+++ src/main/java/it/polimi/ingsw/Example.java
@@ -3,6 +3,8 @@ package it.polimi.ingsw;
 public class Example {
     public static void main (String[] args){
         boolean [][][] map = new boolean[5][5][3];
+        int num = 55;
+
 
         System.out.println("Hello World prova Dario!");
     }
