def37dd672b589f03edbfaf27637830933243346
Vasio1298 <darioalex.gervasio@mail.polimi.it>
2020-03-18 15:47:02 +0100

    Prova branch Dario.

--- src/main/java/it/polimi/ingsw/Example.java
+++ src/main/java/it/polimi/ingsw/Example.java
@@ -3,7 +3,9 @@ package it.polimi.ingsw;
 public class Example {
     public static void main (String[] args){
         boolean [][][] map = new boolean[5][5][3];
-
+        int num = 55;
+        
         System.out.println("Hello World prova Dario!");
+
     }
 }
