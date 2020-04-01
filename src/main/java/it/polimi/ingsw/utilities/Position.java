ab37ae410fa0516778db95bced40c3ae9963052e
giusti-leo <leonardo.giusti@mail.polimi.it>
2020-04-01 23:01:02 +0200

    Aggiornamenti Position

--- src/main/java/it/polimi/ingsw/utilities/Position.java
+++ src/main/java/it/polimi/ingsw/utilities/Position.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.utilities;
 
 public class Position {
+
     private int x, y;
     public int getX(){
         return this.x;
@@ -9,10 +10,120 @@ public class Position {
         return this.y;
     }
 
-    public void set(int x, int y){
+    public Position create(int x, int y){
         this.x = x;
         this.y = y;
+        return this;
     }
 
+    /**
+     * method that give the Position to the right of the given position
+     *
+     * @param position  current position
+     * @return  true if the position is correct, false if the position is incorrect
+     */
+     public Position toRight(Position position){
+        Position newPosition = create(position.getX()+1,position.getY());
+        if(check(newPosition)) return newPosition;
+        else return null;
+    }
+
+    /**
+     * method that give the Position to the left of the given position
+     *
+     * @param position  current position
+     * @return  true if the position is correct, false if the position is incorrect
+     */
+    public Position toLeft(Position position){
+        Position newPosition = create(position.getX()-1, position.getY());
+        if(check(newPosition)) return newPosition;
+        else return null;
+    }
+
+    /**
+     * method that give the Position next to the given position
+     *
+     * @param position  current position
+     * @return  true if the position is correct, false if the position is incorrect
+     */
+    public Position nextTo(Position position){
+        Position newPosition = create( position.getX(),position.getY()+1);
+        if(check(newPosition)) return newPosition;
+        else return null;
+    }
+
+    /**
+     * method that give the Position behind the given position
+     *
+     * @param position  current position
+     * @return  true if the position is correct, false if the position is incorrect
+     */
+    public Position behind(Position position){
+        Position newPosition = create(position.getX(),position.getY()-1);
+        if(check(newPosition)) return newPosition;
+        else return null;
+    }
+
+    /**
+     * method that give the Position to NordEast of the given position
+     *
+     * @param position  current position
+     * @return  true if the position is correct, false if the position is incorrect
+     */
+    public Position toNordEast(Position position){
+        Position newPosition = create(position.getX()+1,position.getY()+1);
+        if(check(newPosition)) return newPosition;
+        else return null;
+    }
+
+    /**
+     * method that give the Position to NordWest of the given position
+     *
+     * @param position  current position
+     * @return  true if the position is correct, false if the position is incorrect
+     */
+    public Position toNordWest(Position position){
+        Position newPosition = create(position.getX()-1,position.getY()+1);
+        if(check(newPosition)) return newPosition;
+        else return null;
+    }
+
+    /**
+     * method that give the Position to SouthEast of the given position
+     *
+     * @param position  current position
+     * @return  true if the position is correct, false if the position is incorrect
+     */
+    public Position toSouthEast(Position position){
+        Position newPosition = create(position.getX()+1,position.getY()-1);
+        if(check(newPosition)) return newPosition;
+        else return null;
+    }
+
+    /**
+     * method that give the Position to NordEst of the given position
+     *
+     * @param position  current position
+     * @return  true if the position is correct, false if the position is incorrect
+     */
+    public Position toSouthWest(Position position){
+        Position newPosition = create(position.getX()-1,position.getY()-1);
+        if(check(newPosition)) return newPosition;
+        else return null;
+    }
+
+    /**
+     * method that control if the position is correct
+     * a position is correct if position.x and position.y =<4 & >=0
+     *
+     * @param position  current position
+     * @return  true if the position is correct, false if the position is incorrect
+     */
+    public boolean check(Position position){
+        if(position.getX()>=0 && position.getX()<=4 && position.getY()>=0 && position.getY()<=4)
+            return true;
+        else
+            return false;
+    }
 }
 
