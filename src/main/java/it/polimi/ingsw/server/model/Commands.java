f79ab9ebcfd09dc44e7227d65cd34449ff604bff
hamzahaddaoui <hamza.haddaoui@mail.polimi.it>
2020-04-02 10:56:14 +0200

    Changes in  Commands, CommandsDecorator

--- src/main/java/it/polimi/ingsw/server/model/Commands.java
+++ src/main/java/it/polimi/ingsw/server/model/Commands.java
@@ -1,12 +1,9 @@
 package it.polimi.ingsw.server.model;
 
 import it.polimi.ingsw.utilities.Position;
-
-import java.util.List;
 import java.util.Set;
 
 public interface Commands {
-
     public void placeWorker(Position position, Player player);
 
     public void moveWorker(Position position, Player player);
@@ -16,7 +13,4 @@ public interface Commands {
     public Set<Position> getAvailableCells(Player player);
 
     public void specialFunctionSetUnset(Player player);
-
-
-
 }
