925dd608c006d22709296c098ad41d8e00d11235
hamzahaddaoui <hamza.haddaoui@mail.polimi.it>
2020-04-06 23:49:26 +0200

    Fixed hash function and equals for Position Added test coverage for Billboard and Position

--- src/test/java/it/polimi/ingsw/server/model/BillboardTest.java
+++ src/test/java/it/polimi/ingsw/server/model/BillboardTest.java
@@ -5,59 +5,54 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BillboardTest {
-
+    Billboard billboard = new Billboard();
+    Position position;
+    Random random = new Random();
     @BeforeEach
     void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
+        position = new Position(Math.abs(random.nextInt()%5),Math.abs(random.nextInt()%5));
     }
 
     @Test
-    void getTowerHeight() {
-        Billboard billboard = new Billboard();
-        billboard.incrementTowerHeight(new Position(2,3));
-        assertEquals(1, billboard.getTowerHeight(new Position (2,3)),"ok");
-
-    }
-
-    @Test
-    void testGetTowerHeight() {
-    }
-
-    @Test
-    void incrementTowerHeight() {
-    }
-
-    @Test
-    void getDome() {
-    }
-
-    @Test
-    void testGetDome() {
+    void testIncrementTowerHeight() {
+        assertEquals(0, billboard.getTowerHeight(position));
+        billboard.incrementTowerHeight(position);
+        assertEquals(1, billboard.getTowerHeight(position));
+        billboard.incrementTowerHeight(position);
+        assertEquals(2, billboard.getTowerHeight(position));
+        billboard.incrementTowerHeight(position);
+        assertEquals(3, billboard.getTowerHeight(position));
+        assertEquals(false, billboard.getDome(position));
+        billboard.incrementTowerHeight(position);
+        assertEquals(3, billboard.getTowerHeight(position));
+        assertEquals(true, billboard.getDome(position));
     }
 
     @Test
     void setDome() {
-    }
-
-    @Test
-    void getPlayer() {
-    }
-
-    @Test
-    void testGetPlayer() {
+        assertEquals(false, billboard.getDome(position));
+        billboard.setDome(position);
+        assertEquals(true, billboard.getDome(position));
+        assertEquals(true, billboard.getDome().get(position));
     }
 
     @Test
     void setPlayer() {
+        assertEquals(-1, billboard.getPlayer(position));
+        billboard.setPlayer(position, 10);
+        assertEquals(10, billboard.getPlayer(position));
+        assertEquals(10, billboard.getPlayer().get(position));
     }
 
     @Test
     void resetPlayer() {
+        billboard.setPlayer(position, 10);
+        billboard.resetPlayer(position);
+        assertEquals(-1, billboard.getPlayer(position));
+        assertEquals(-1, billboard.getPlayer().get(position));
     }
 }
--- src/test/java/it/polimi/ingsw/utilities/PositionTest.java
+++ src/test/java/it/polimi/ingsw/utilities/PositionTest.java
@@ -4,35 +4,86 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PositionTest {
+    Position position;
 
-    @BeforeEach
-    void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
+    @Test
+    void testNeighbourPositionsCentered() {
+        Set<Position> returnValue = new HashSet<>();
+        position= new Position(3,3);
+        returnValue.add(new Position(2,2));
+        returnValue.add(new Position(2,3));
+        returnValue.add(new Position(2,4));
+        returnValue.add(new Position(3,2));
+        returnValue.add(new Position(3,4));
+        returnValue.add(new Position(4,2));
+        returnValue.add(new Position(4,3));
+        returnValue.add(new Position(4,4));
+        assertEquals(returnValue, position.neighbourPositions());
     }
 
     @Test
-    void neighbourPositions() {
+    void testNeighbourPositionsUpLeftCorner() {
+        Set<Position> returnValue = new HashSet<>();
+        position= new Position(0,0);
+        returnValue.add(new Position(1,1));
+        returnValue.add(new Position(0,1));
+        returnValue.add(new Position(1,0));
+        assertEquals(returnValue, position.neighbourPositions());
     }
 
     @Test
-    void testNeighbourPositions() {
+    void testNeighbourPositionsDownLeftCorner() {
+        Set<Position> returnValue = new HashSet<>();
+        position= new Position(4,0);
+        returnValue.add(new Position(3,1));
+        returnValue.add(new Position(4,1));
+        returnValue.add(new Position(3,0));
+        assertEquals(returnValue, position.neighbourPositions());
     }
 
     @Test
-    void checkMutualPosition() {
+    void testNeighbourPositionsUpRightCorner() {
+        Set<Position> returnValue = new HashSet<>();
+        position= new Position(0,4);
+        returnValue.add(new Position(0,3));
+        returnValue.add(new Position(1,3));
+        returnValue.add(new Position(1,4));
+        assertEquals(returnValue, position.neighbourPositions());
     }
 
     @Test
-    void partialequals() {
+    void testNeighbourPositionsDownRightCorner() {
+        Set<Position> returnValue = new HashSet<>();
+        position= new Position(4,4);
+        returnValue.add(new Position(3,3));
+        returnValue.add(new Position(3,4));
+        returnValue.add(new Position(4,3));
+        assertEquals(returnValue, position.neighbourPositions());
+    }
+
+
+    @Test
+    void testCheckMutualPosition() {
+        position= new Position(3,3);
+        assertEquals(CardinalDirection.NORTH, position.checkMutualPosition(new Position(2,3)));
+        assertEquals(CardinalDirection.SOUTH, position.checkMutualPosition(new Position(4,3)));
+        assertEquals(CardinalDirection.WEST, position.checkMutualPosition(new Position(3,2)));
+        assertEquals(CardinalDirection.EAST, position.checkMutualPosition(new Position(3,4)));
+        assertEquals(CardinalDirection.NORTHWEST, position.checkMutualPosition(new Position(2,2)));
+        assertEquals(CardinalDirection.NORTHEAST, position.checkMutualPosition(new Position(2,4)));
+        assertEquals(CardinalDirection.SOUTHWEST, position.checkMutualPosition(new Position(4,2)));
+        assertEquals(CardinalDirection.SOUTHEAST, position.checkMutualPosition(new Position(4,4)));
     }
 
     @Test
-    void testEquals() {
+    void testMutualPositionNotNeighbouring() {
+        position= new Position(3,3);
+        assertEquals(null, position.checkMutualPosition(new Position(1,1)));
     }
 }
