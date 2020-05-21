3e1a5133abf1866726a5b74e28ce726a5483c756
hamzahaddaoui <hamza.haddaoui@mail.polimi.it>
2020-05-21 23:48:27 +0200

    GUI fix

--- src/main/java/it/polimi/ingsw/GUI/controller/Running.java
+++ src/main/java/it/polimi/ingsw/GUI/controller/Running.java
@@ -170,7 +170,6 @@ public class Running extends State{
                 System.out.println("MOVE OK. SENDING movement...");
                 getIslandLoader().moveWorker(positionToPoint(getStartingPosition()), point);
                 setEndPosition(position);
-                setStartingPosition(getStartingPosition());
                 sendData();
             }
         }
