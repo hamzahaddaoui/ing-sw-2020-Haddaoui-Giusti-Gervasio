f8627c40c2e7e2ee1cf2ed525c5fdcaf230a8f2b
hamzahaddaoui <hamza.haddaoui@mail.polimi.it>
2020-05-24 17:52:38 +0200

    GUI added WIN & LOST screen GUI added WaitingList

--- src/main/java/it/polimi/ingsw/utilities/Observable.java
+++ src/main/java/it/polimi/ingsw/utilities/Observable.java
@@ -8,15 +8,11 @@ public class Observable<T> {
     private final List<Observer<T>> observers = new ArrayList<>();
 
     public void addObserver(Observer<T> observer){
-        synchronized (observers) {
             observers.add(observer);
-        }
     }
 
     public void removeObserver(Observer<T> observer){
-        synchronized (observers) {
             observers.remove(observer);
-        }
     }
 
     public List<Observer<T>> getObservers(){
@@ -24,19 +20,15 @@ public class Observable<T> {
     }
 
     protected void notify(T message){
-        synchronized (observers) {
             for(Observer<T> observer : observers){
                 observer.update(message);
             }
-        }
     }
 
     protected void notify(List<Observer<T>> observerList, T message){
-        synchronized (observerList) {
             for(Observer<T> observer : observerList){
                 observer.update(message);
             }
-        }
     }
 
 
