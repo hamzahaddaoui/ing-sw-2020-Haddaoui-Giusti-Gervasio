1df55640f8a7aed04c2351e62f5f254b3ffb7b8c
hamzahaddaoui <hamza.haddaoui@mail.polimi.it>
2020-05-26 16:00:19 +0200

    GUI fix

--- src/main/java/it/polimi/ingsw/GUI/Application.java
+++ src/main/java/it/polimi/ingsw/GUI/Application.java
@@ -4,7 +4,9 @@ import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args){
-        System.out.println(Arrays.toString(args));
+        if (args[0].equals("-cli")){
+            //run cli
+        }
         Client.main(args);
     }
 }
