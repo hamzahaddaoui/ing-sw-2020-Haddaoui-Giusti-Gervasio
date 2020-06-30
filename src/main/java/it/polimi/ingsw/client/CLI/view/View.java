973069e5d780d09dd44c8e4527e3e1393653be71
giusti_leo <leonardo.giusti@mail.polimi.it>
2020-07-01 00:50:20 +0200

    modifiche view per compatibilita` windows mac

--- src/main/java/it/polimi/ingsw/client/CLI/view/View.java
+++ src/main/java/it/polimi/ingsw/client/CLI/view/View.java
@@ -111,16 +111,17 @@ public class View implements Observer<MessageEvent> {
         final String ANSI_PURPLE = "\u001B[35m";
         final String ANSI_RED = "\u001B[31m";
         final String ANSI_WHITE = "\u001B[37m";
+        final String ANSI_BLACK = "\u001B[30m";
 
         billboardCells
                 .keySet()
                 .stream()
                 .sorted()
                 .forEach(position -> outputA
-                        .append(billboardCells.get(position).getPlayerID() == 0 ? ANSI_WHITE +  "⬜" + ANSI_RESET : "") //  ⬜️
-                        .append(billboardCells.get(position).getPlayerID() != 0 && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Blue") ? ANSI_BLUE + "🟦" + ANSI_RESET : "")
-                        .append(billboardCells.get(position).getPlayerID() != 0 && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Orange") ? ANSI_RED + "🟧" + ANSI_RESET : "") //🟩
-                        .append(billboardCells.get(position).getPlayerID() != 0 && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Purple") ? ANSI_PURPLE + "🟪" + ANSI_RESET : "") //🟦
+                        .append(billboardCells.get(position).getPlayerID() == 0 ? ANSI_BLACK + "[" + ANSI_WHITE + "\u25a0" + ANSI_BLACK + "]"+ ANSI_RESET : "") //  ⬜️
+                        .append(billboardCells.get(position).getPlayerID() != 0 && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Blue") ? ANSI_BLACK + "[" + ANSI_BLUE + "\u25a0" + ANSI_BLACK + "]" + ANSI_RESET : "")
+                        .append(billboardCells.get(position).getPlayerID() != 0 && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Orange") ? ANSI_BLACK + "[" + ANSI_RED + "\u25a0" + ANSI_BLACK + "]" + ANSI_RESET : "") //🟩
+                        .append(billboardCells.get(position).getPlayerID() != 0 && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Purple") ? ANSI_BLACK + "[" + ANSI_PURPLE + "\u25a0" + ANSI_BLACK + "]" + ANSI_RESET : "") //🟦
                         .append((position.getY() == 4) ? "\n" : " "));
 
         outputA.append("\n");
@@ -136,6 +137,9 @@ public class View implements Observer<MessageEvent> {
     static String getPlacingAvailableCells(Set<Position> cells) {
         StringBuilder outputB = new StringBuilder();
 
+        final String ANSI_WHITE = "\u001B[37m";
+        final String ANSI_BLACK = "\u001B[30m";
+
         Map<Position,Cell> billboardCells = DataBase.getBillboardStatus();
 
         billboardCells
@@ -143,8 +147,8 @@ public class View implements Observer<MessageEvent> {
                 .stream()
                 .sorted()
                 .forEach(position -> outputB
-                        .append(cells.contains(position) ? "\u2B1B" : "" )
-                        .append(!cells.contains(position) ? "\u2B1C" : "")
+                        .append(cells.contains(position) ? ANSI_BLACK + "[" + ANSI_BLACK + "\u25a0" + ANSI_BLACK + "]" : "" )
+                        .append(!cells.contains(position) ? ANSI_BLACK + "[" + ANSI_WHITE + "\u25a0" + ANSI_BLACK + "]" : "")
                         .append((position.getY() == 4) ? "\n" : " "));
         outputB.append("\n");
         return outputB.toString();
@@ -187,16 +191,28 @@ public class View implements Observer<MessageEvent> {
     static String getBillboardStat( Set<Position> cells, Position p){
         StringBuilder outputC = new StringBuilder();
 
+        final String ANSI_WHITE = "\u001B[37m";
+        final String ANSI_BLACK = "\u001B[30m";
+        final String ANSI_BLUE = "\u001B[34m";
+        final String ANSI_PURPLE = "\u001B[35m";
+        final String ANSI_RED = "\u001B[31m";
+
         Map<Position, Cell> billboardCells = DataBase.getBillboardStatus();
+        Map<Integer,String> playerColors = DataBase.getMatchColors();
 
         billboardCells
                 .keySet()
                 .stream()
                 .sorted()
                 .forEach(position -> outputC
-                        .append(cells.contains(position) ? "\u2B1B" : "")
-                        .append(! (p.equals(position)) && ! cells.contains(position) ? "\u2B1C" : "")
-                        .append((p.equals(position)) ? "\uD83D\uDC77\uD83C\uDFFB" : "")
+                        .append(cells.contains(position) && !p.equals(position) ? ANSI_BLACK + "[\u25a0]" : "")
+                        .append(! (p.equals(position)) && ! cells.contains(position) ? ANSI_BLACK + "[" + ANSI_WHITE + "\u25a0" + ANSI_BLACK + "]" : "")
+                        .append((p.equals(position) && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Blue") && cells.contains(position)) ? ANSI_BLACK + "[" + ANSI_BLUE + "\u265f" + ANSI_BLACK + "\u25a0]" : "")
+                        .append((p.equals(position) && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Orange") && cells.contains(position)) ? ANSI_BLACK + "[" + ANSI_RED + "\u265f" + ANSI_BLACK + "\u25a0]" : "")
+                        .append((p.equals(position) && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Purple") && cells.contains(position)) ? ANSI_BLACK + "[" + ANSI_PURPLE + "\u265f" + ANSI_BLACK + "\u25a0]" : "")
+                        .append((p.equals(position) && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Blue") && !cells.contains(position)) ? ANSI_BLACK + "[" + ANSI_BLUE + "\u265f" + ANSI_BLACK + "]" : "")
+                        .append((p.equals(position) && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Orange") && !cells.contains(position)) ? ANSI_BLACK + "[" + ANSI_RED + "\u265f" + ANSI_BLACK + "]" : "")
+                        .append((p.equals(position) && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Purple") && !cells.contains(position)) ? ANSI_BLACK + "[" + ANSI_PURPLE + "\u265f" + ANSI_BLACK + "]" : "")
                         .append((position.getY() == 4) ? "\n" : " "));
 
         outputC.append("\n");
@@ -212,6 +228,9 @@ public class View implements Observer<MessageEvent> {
     static String getBillboardEvidence( Set<Position> cells){
         StringBuilder outputD = new StringBuilder();
 
+        final String ANSI_WHITE = "\u001B[37m";
+        final String ANSI_BLACK = "\u001B[30m";
+
         Map<Position, Cell> billboardCells = DataBase.getBillboardStatus();
 
         billboardCells
@@ -219,8 +238,8 @@ public class View implements Observer<MessageEvent> {
                 .stream()
                 .sorted()
                 .forEach(position -> outputD
-                        .append(cells.contains(position) ? "\u2B1B" : "")
-                        .append(! cells.contains(position) ? "\u2B1C" : "")
+                        .append(cells.contains(position) ? ANSI_BLACK + "[" + ANSI_BLACK + "\u25a0" + ANSI_BLACK + "]" : "")
+                        .append(! cells.contains(position) ? ANSI_BLACK + "[" + ANSI_WHITE + "\u25a0" + ANSI_BLACK + "]" : "")
                         .append((position.getY() == 4) ? "\n" : " "));
 
         outputD.append("\n");
@@ -243,29 +262,31 @@ public class View implements Observer<MessageEvent> {
        final String ANSI_PURPLE = "\u001B[35m";
        final String ANSI_RED = "\u001B[31m";
        final String ANSI_WHITE = "\u001B[37m";
+       final String ANSI_BLACK = "\u001B[30m";
+       final String ANSI_YELLOW = "\u001B[33m";
 
         billboardCells
                 .keySet()
                 .stream()
                 .sorted()
                 .forEach(position -> output
-                        .append(billboardCells.get(position).isDome() && billboardCells.get(position).getPlayerID() == 0 ? ANSI_WHITE + "⬜" + ANSI_RESET + "⏺ " : "")
-                        .append(!billboardCells.get(position).isDome() && billboardCells.get(position).getTowerHeight() == 0 && billboardCells.get(position).getPlayerID() == 0 ? ANSI_WHITE + "⬜" + ANSI_RESET + "0️⃣" : "")
-                        .append(!billboardCells.get(position).isDome() && billboardCells.get(position).getTowerHeight() == 0 && billboardCells.get(position).getPlayerID() != 0 && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Blue") ? ANSI_BLUE + "🟦" + ANSI_RESET + "0️⃣" : "")
-                        .append(!billboardCells.get(position).isDome() && billboardCells.get(position).getTowerHeight() == 0 && billboardCells.get(position).getPlayerID() != 0 && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Orange") ? ANSI_RED + "🟧" + ANSI_RESET + "0️⃣" : "")
-                        .append(!billboardCells.get(position).isDome() && billboardCells.get(position).getTowerHeight() == 0 && billboardCells.get(position).getPlayerID() != 0 && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Purple") ? ANSI_PURPLE + "🟪" + ANSI_RESET + "0️⃣" : "")
-                        .append(!billboardCells.get(position).isDome() && billboardCells.get(position).getTowerHeight() == 1 && billboardCells.get(position).getPlayerID() == 0 ? ANSI_WHITE + "⬜" + ANSI_RESET + "1️⃣" : "")
-                        .append(!billboardCells.get(position).isDome() && billboardCells.get(position).getTowerHeight() == 1 && billboardCells.get(position).getPlayerID() != 0 && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Blue") ? ANSI_BLUE + "🟦" + ANSI_RESET + "1️⃣" : "")
-                        .append(!billboardCells.get(position).isDome() && billboardCells.get(position).getTowerHeight() == 1 && billboardCells.get(position).getPlayerID() != 0 && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Orange") ? ANSI_RED + "🟧" + ANSI_RESET + "1️⃣" : "")
-                        .append(!billboardCells.get(position).isDome() && billboardCells.get(position).getTowerHeight() == 1 && billboardCells.get(position).getPlayerID() != 0 && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Purple") ? ANSI_PURPLE + "🟪" + ANSI_RESET + "1️⃣" : "")
-                        .append(!billboardCells.get(position).isDome() && billboardCells.get(position).getTowerHeight() == 2 && billboardCells.get(position).getPlayerID() == 0 ? ANSI_WHITE + "⬜" + ANSI_RESET + "2️⃣" : "")
-                        .append(!billboardCells.get(position).isDome() && billboardCells.get(position).getTowerHeight() == 2 && billboardCells.get(position).getPlayerID() != 0 && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Blue") ? ANSI_BLUE + "🟦" + ANSI_RESET + "2️⃣" : "")
-                        .append(!billboardCells.get(position).isDome() && billboardCells.get(position).getTowerHeight() == 2 && billboardCells.get(position).getPlayerID() != 0 && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Orange") ? ANSI_RED + "🟧" + ANSI_RESET + "2️⃣" : "")
-                        .append(!billboardCells.get(position).isDome() && billboardCells.get(position).getTowerHeight() == 2 && billboardCells.get(position).getPlayerID() != 0 && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Purple") ? ANSI_PURPLE + "🟪" + ANSI_RESET + "2️⃣" : "")
-                        .append(!billboardCells.get(position).isDome() && billboardCells.get(position).getTowerHeight() == 3 && billboardCells.get(position).getPlayerID() == 0 ? ANSI_WHITE + "⬜" + ANSI_RESET + "3️⃣" : "")
-                        .append(!billboardCells.get(position).isDome() && billboardCells.get(position).getTowerHeight() == 3 && billboardCells.get(position).getPlayerID() != 0 && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Blue") ? ANSI_BLUE + "🟦" + ANSI_RESET + "3️⃣" : "")
-                        .append(!billboardCells.get(position).isDome() && billboardCells.get(position).getTowerHeight() == 3 && billboardCells.get(position).getPlayerID() != 0 && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Orange") ? ANSI_RED + "🟧" + ANSI_RESET + "3️⃣" : "")
-                        .append(!billboardCells.get(position).isDome() && billboardCells.get(position).getTowerHeight() == 3 && billboardCells.get(position).getPlayerID() != 0 && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Purple") ? ANSI_PURPLE + "🟪" + ANSI_RESET + "3️⃣" : "")
+                        .append(billboardCells.get(position).isDome() && billboardCells.get(position).getPlayerID() == 0 ? ANSI_BLACK + "[" + ANSI_WHITE + "\u25a0" + ANSI_BLACK + "]" + ANSI_BLACK + "[" + ANSI_YELLOW + "\u25B2" + ANSI_BLACK + "]" : "")
+                        .append(!billboardCells.get(position).isDome() && billboardCells.get(position).getTowerHeight() == 0 && billboardCells.get(position).getPlayerID() == 0 ? ANSI_BLACK + "[" + ANSI_WHITE + "\u25a0" + ANSI_BLACK + "]" + ANSI_RESET + "[0]" : "")
+                        .append(!billboardCells.get(position).isDome() && billboardCells.get(position).getTowerHeight() == 0 && billboardCells.get(position).getPlayerID() != 0 && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Blue") ? ANSI_BLACK + "[" + ANSI_BLUE + "\u25a0" + ANSI_BLACK + "]"  + ANSI_RESET + "[0]" : "")
+                        .append(!billboardCells.get(position).isDome() && billboardCells.get(position).getTowerHeight() == 0 && billboardCells.get(position).getPlayerID() != 0 && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Orange") ? ANSI_BLACK + "[" +ANSI_RED + "\u25a0" + ANSI_BLACK + "]"  + ANSI_RESET + "[0]" : "")
+                        .append(!billboardCells.get(position).isDome() && billboardCells.get(position).getTowerHeight() == 0 && billboardCells.get(position).getPlayerID() != 0 && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Purple") ? ANSI_BLACK + "[" + ANSI_PURPLE + "\u25a0" + ANSI_BLACK + "]"  + ANSI_RESET + "[0]" : "")
+                        .append(!billboardCells.get(position).isDome() && billboardCells.get(position).getTowerHeight() == 1 && billboardCells.get(position).getPlayerID() == 0 ? ANSI_BLACK + "[" + ANSI_WHITE + "\u25a0" + ANSI_BLACK + "]" + ANSI_RESET + "[1]" : "")
+                        .append(!billboardCells.get(position).isDome() && billboardCells.get(position).getTowerHeight() == 1 && billboardCells.get(position).getPlayerID() != 0 && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Blue") ? ANSI_BLACK + "[" + ANSI_BLUE + "\u25a0" + ANSI_BLACK + "]"  + ANSI_RESET + "[1]" : "")
+                        .append(!billboardCells.get(position).isDome() && billboardCells.get(position).getTowerHeight() == 1 && billboardCells.get(position).getPlayerID() != 0 && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Orange") ? ANSI_BLACK + "[" + ANSI_RED + "\u25a0" + ANSI_BLACK + "]"  + ANSI_RESET + "[1]" : "")
+                        .append(!billboardCells.get(position).isDome() && billboardCells.get(position).getTowerHeight() == 1 && billboardCells.get(position).getPlayerID() != 0 && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Purple") ? ANSI_BLACK + "[" + ANSI_PURPLE + "\u25a0" + ANSI_BLACK + "]"  + ANSI_RESET + "[1]" : "")
+                        .append(!billboardCells.get(position).isDome() && billboardCells.get(position).getTowerHeight() == 2 && billboardCells.get(position).getPlayerID() == 0 ? ANSI_BLACK + "[" + ANSI_WHITE + "\u25a0" + ANSI_BLACK + "]" + ANSI_RESET + "[2]" : "")
+                        .append(!billboardCells.get(position).isDome() && billboardCells.get(position).getTowerHeight() == 2 && billboardCells.get(position).getPlayerID() != 0 && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Blue") ? ANSI_BLACK + "[" + ANSI_BLUE + "\u25a0" + ANSI_BLACK + "]"  + ANSI_RESET + "[2]" : "")
+                        .append(!billboardCells.get(position).isDome() && billboardCells.get(position).getTowerHeight() == 2 && billboardCells.get(position).getPlayerID() != 0 && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Orange") ? ANSI_BLACK + "[" + ANSI_RED + "\u25a0" + ANSI_BLACK + "]"  + ANSI_RESET + "[2]" : "")
+                        .append(!billboardCells.get(position).isDome() && billboardCells.get(position).getTowerHeight() == 2 && billboardCells.get(position).getPlayerID() != 0 && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Purple") ? ANSI_BLACK + "[" + ANSI_PURPLE + "\u25a0" + ANSI_BLACK + "]"  + ANSI_RESET + "[2]" : "")
+                        .append(!billboardCells.get(position).isDome() && billboardCells.get(position).getTowerHeight() == 3 && billboardCells.get(position).getPlayerID() == 0 ? ANSI_BLACK + "[" + ANSI_WHITE + "\u25a0" + ANSI_BLACK + "]" + ANSI_RESET + "[3]" : "")
+                        .append(!billboardCells.get(position).isDome() && billboardCells.get(position).getTowerHeight() == 3 && billboardCells.get(position).getPlayerID() != 0 && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Blue") ? ANSI_BLACK + "[" + ANSI_BLUE + "\u25a0" + ANSI_BLACK + "]"  + ANSI_RESET + "[3]" : "")
+                        .append(!billboardCells.get(position).isDome() && billboardCells.get(position).getTowerHeight() == 3 && billboardCells.get(position).getPlayerID() != 0 && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Orange") ? ANSI_BLACK + "[" + ANSI_RED + "\u25a0" + ANSI_BLACK + "]"   + ANSI_RESET + "[3]" : "")
+                        .append(!billboardCells.get(position).isDome() && billboardCells.get(position).getTowerHeight() == 3 && billboardCells.get(position).getPlayerID() != 0 && playerColors.get(billboardCells.get(position).getPlayerID()).equals("Purple") ? ANSI_BLACK + "[" + ANSI_PURPLE + "\u25a0" + ANSI_BLACK + "]"   + ANSI_RESET + "[3]" : "")
                         .append((position.getY() == 4) ? "\n" : " "));
 
         output.append("\n");
